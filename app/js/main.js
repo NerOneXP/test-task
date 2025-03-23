60307073414c506ad74248ada6d64a78cd66f9f2
NerOneXP <ilaa16119@gmail.com>
2025-03-23 15:41:43 +0300

    v1

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,7 @@ $(function () {
       $(".top-info__btn").not(this).removeClass("active");
 
       content.toggleClass("active");
+      $('.header').css('z-index', '2');
       $(this).toggleClass("active");
     });
 
@@ -44,20 +45,23 @@ $(function () {
     });
   });
 
-  
+
   const swiper = new Swiper('.slider__swipe', {
-    slidesPerView: 3,
-    Centered: true,
+    slidesPerView: 1,
+
     spaceBetween: 37,
     navigation: {
       nextEl: ".slider-button-next",
       prevEl: ".slider-button-prev",
     },
-    breakpoints: { // адаптация под разные размеры экрана
-      1200: {
-        slidesPerView: 2, // на экранах больше 1200px будет показываться 2 слайда
+    breakpoints:{
+      640:{
+        slidesPerView: 2,
       },
-    },
+      1050:{
+        slidesPerView: 3,
+      },
+    }
   });
   const swiperpopup = new Swiper('.product-popup__imgbox', {
     slidesPerView: 1,
@@ -71,14 +75,23 @@ $(function () {
   });
 
   const swiperproduct = new Swiper('.product-content', {
-    slidesPerView: 4,
-    Centered: true,
-    breakpoints: { // адаптация под разные размеры экрана
-    },
+    slidesPerView: 1,
+    spaceBetween: 41,
+    breakpoints:{
+      777:{
+        slidesPerView: 2,
+      },
+      1150:{
+        slidesPerView: 3,
+      },
+      1550:{
+        slidesPerView: 4,
+      },
+    }
   });
 
 
-  
+
   $(document).ready(function () {
     $(".block-size__label").click(function () {
       $(".block-size__label").removeClass("block-size__label--active");
@@ -87,26 +100,35 @@ $(function () {
   });
 
   $('.menu__btn').on('click', function () {
+    $('.header, .top-info').css('z-index', '9');
     $('.overlay').toggleClass('overlay--active');
     $('.menu').toggleClass('menu--active');
+    $('body').toggleClass('no-scroll');
+    $('.menu__btn').toggleClass('menu__btn--active');
+  });
+  
+  $('.Etiam-btn, .rhoncus-btn').on('click', function () {
+    $('.header, .top-info').css('z-index', '7');
+    $('body').addClass('no-scroll');
+    $('.overlay').toggleClass('overlay--active');
+
+    let targetPopup = $(this).hasClass('Etiam-btn') ? '.product-popup' : '.form-popup';
+    $(targetPopup).toggleClass(targetPopup.substring(1) + '--active');
   });
 
-  $('.Etiam-btn').on('click', function () {
-    $('.overlay').toggleClass('overlay--active');
-    $('.product-popup').toggleClass('product-popup--active');
-  });
-  $('.rhoncus-btn').on('click', function () {
-    $('.overlay').toggleClass('overlay--active');
-    $('.form-popup').toggleClass('form-popup--active');
-  });
   $('.popup__btn').on('click', function () {
-    $('.overlay').removeClass('overlay--active');
-    $('.product-popup').removeClass('product-popup--active');
-  });
-  $('.popup__btn').on('click', function () {
-    $('.overlay').removeClass('overlay--active');
-    $('.form-popup').removeClass('form-popup--active');
+    $('body').removeClass('no-scroll');
+    $('.overlay, .product-popup, .form-popup').removeClass('overlay--active product-popup--active form-popup--active');
   });
 
+  $(document).on("click", ".block-size__quantity-btn", function (e) {
+    e.preventDefault();
+
+    let $value = $(this).siblings("[data-value]"),
+        val = parseInt($value.text()) || 1,
+        newVal = $(this).data("action") === "minus" ? Math.max(1, val - 1) : val + 1;
+
+    $value.text(newVal);
+});
 
 });
